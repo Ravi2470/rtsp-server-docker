b02ebcdcd9fa15a8f95258450c3e57f9d6f647bc
Ravi2470 <109647098+Ravi2470@users.noreply.github.com>
2023-03-29 16:04:13 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -6,7 +6,7 @@ ffmpeg -re -stream_loop -1 -i sample-5s.mp4 -c copy -f rtsp rtsp://localhost:854
 
 ###
 
-ffmpeg -re -i rtsp://username:password@ip_address:port/cam/realmonitor?channel=1&subtype=0 -c copy -f rtsp rtsp://10.20.20.231:8543/video
+ffmpeg -re -i rtsp://username:password@ip_address:port/cam/realmonitor?channel=1&subtype=0 -c copy -f rtsp rtsp://localhost:8543/video
 
 In this command, you need to replace username, password, ip_address, and port with the actual values of your IP camera. 
 The channel and subtype parameters are optional and depend on your camera's configuration. 
